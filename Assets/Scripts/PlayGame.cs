06451b352acdc2c3620162b2edc227ee50359dda
wascus <fabiani.1862447@studenti.uniroma1.it>
2024-09-26 11:12:53 +0200

    menu + ui

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PlayGame : MonoBehaviour
+{
+    public void PlayGame_()
+    {
+        SceneManager.LoadScene("BasicTestingScene"); // Replace with the name of your game scene
+    }
+}
